a08c39322cdbff78bb747a093f8e2e5abacb03c8
edubew <edubew98@gmail.com>
2023-09-06 22:54:51 +0300

    Modify the styling

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,39 +1,65 @@
 const tableScores = document.querySelector('.table-scores');
 const name = document.querySelector('.name');
 const score = document.querySelector('.scores');
+const apiUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/lLwxzvstdE0zBWjxl3BY/scores';
 
+// Function to display game scores
 export const getGameScores = (gamescore) => {
   let output = '';
-  gamescore.map((score) => {
+  // Clear the table before adding new scores
+  tableScores.innerHTML = '';
+  gamescore.forEach((score) => {
     output += `
-    <tr>
-     <td>${score.user}</td>
-     <td>${score.score}</td>
-    </tr>
+        <li>${score.user}:
+        <span>${score.score}%</span></li>
     `;
-    tableScores.innerHTML = output;
   });
+  tableScores.innerHTML = output;
 };
 
+// Function to post data to the API
 export const postData = async () => {
-  await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/pYFkaMcA8mcPphvtdGOB/scores',
-    {
-      method: 'POST',
-      body: JSON.stringify({
-        user: name.value,
-        score: score.value,
-        index: Date.now(),
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    });
-  name.value = '';
-  score.value = '';
+  const userScore = parseInt(score.value, 10);
+
+  if (userScore <= 100) {
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        body: JSON.stringify({
+          user: name.value,
+          score: userScore,
+          index: Date.now(),
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+
+      if (response.ok) {
+        name.value = '';
+        score.value = '';
+      } else {
+        throw new Error('Failed to post data to the API');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
+    console.error('Score cannot be more than 100%');
+  }
 };
 
+// Function to fetch API data
 export const getApiData = async () => {
-  const res = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/pYFkaMcA8mcPphvtdGOB/scores');
-  const data = await res.json();
-  getGameScores(data.result);
+  try {
+    const res = await fetch(apiUrl);
+    if (res.ok) {
+      const data = await res.json();
+      getGameScores(data.result);
+    } else {
+      throw new Error('Failed to fetch data from the API');
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
